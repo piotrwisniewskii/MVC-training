1569a845e58d9f037cac8077313e4547e8fa003d
Piotr Wisniewski <piowisni@gmail.com>
2023-04-21 11:25:40 +0200

    Identity Added

--- CarWorkshop.Infrastructure/Persistance/CarWorkshopDbContext.cs
+++ CarWorkshop.Infrastructure/Persistance/CarWorkshopDbContext.cs
@@ -2,20 +2,23 @@
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
 using CarWorkshop.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CarWorkshop.Infrastructure.Persistance
 {
-    public class CarWorkshopDbContext : DbContext
+    public class CarWorkshopDbContext : IdentityDbContext
     {
         public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
 
         public CarWorkshopDbContext(DbContextOptions<CarWorkshopDbContext>options) : base(options)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelbuilder)
         {
+            base.OnModelCreating(modelbuilder);
+
             modelbuilder.Entity<Domain.Entities.CarWorkshop>()
                 .OwnsOne(c => c.ContactDetails);
         }
--- CarWorkshop.MVC/Program.cs
+++ CarWorkshop.MVC/Program.cs
@@ -41,4 +41,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
